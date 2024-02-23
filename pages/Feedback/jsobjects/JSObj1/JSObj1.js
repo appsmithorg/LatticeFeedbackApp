31601d6a98ef6e333a5e7359e34b7ecca652fd7f
shadab <shadab@appsmith.com>
2024-02-23 11:33:54 +0000

    commit

--- pages/Feedback/jsobjects/JSObj1/JSObj1.js
+++ pages/Feedback/jsobjects/JSObj1/JSObj1.js
@@ -4,18 +4,18 @@ export default {
 	defaultValue: " ",
 	function1 (text) 
 	{
-    // Get the text from the text widget
-    var textWidget1 = document.getElementById(Text16);
+		// Get the text from the text widget
+		//var textWidget1 = document.getElementById(Text16);
 		// and assign it to a variable
-    //var text = Text16.text;
+		//var text = Text16.text;
 		//It then gets the existing input widget using getElementById
-		var existingInput1 = document.getElementById(RichTextEditor1);
+		var existingInput1 = RichTextEditor1.text;
 		//set the value of the input widget to the value that is in the variable text above.
 
 		//storeValue("defaultValue", "test")
-		this.defaultValue = text
+		this.defaultValue = existingInput1 + text
 
-},
+	},
 	async myFun2 () {
 		//	use async-await or promises
 		//	await storeValue('varName', 'hello world')
